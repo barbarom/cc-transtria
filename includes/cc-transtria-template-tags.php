5e28e7f608861748bb20b2d4357533213948fc7d
Melissa De Bartolomeo <debartolomeo@hotmail.com>
2015-07-15 12:36:42 -0500

    added template file; reduced database file to just a few examples

---
+++ includes/cc-transtria-template-tags.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * CC Transtria Extras
+ *
+ * @package   CC Transtria Extras
+ * @author    CARES staff
+ * @license   GPL-2.0+
+ * @copyright 2015 CommmunityCommons.org
+ */
+/**
+ * Print content for the "Introduction" (default tab)
+ *
+ * @since   1.0.0
+ * @return  HTML
+ */
+function cc_transtria_print_introductory_text(){
+    ?>
+    <p>
+        Welcome to Transtria's Study editor.
+    </p>
+    <?php
+}
+
+
+/**
+ * Builds the subnav of the AHA group tab
+ *
+ * @since   1.0.0
+ * @return  HTML
+ */
+function cc_transtria_render_tab_subnav(){
+        ?>
+        <div id="subnav" class="item-list-tabs no-ajax">
+            <ul class="nav-tabs">
+              <!--  <li <?php if ( cc_aha_on_main_screen() ) { echo 'class="current"'; } ?>>
+                    <a href="<?php echo cc_aha_get_home_permalink(); ?>">Introduction</a>
+                </li>
+				<li <?php if ( cc_aha_on_survey_screen() ) { echo 'class="current"'; } ?>>
+					<a href="<?php echo cc_aha_get_survey_permalink(); ?>">Assessment</a>
+				</li>
+                <li <?php if ( cc_aha_on_analysis_screen( 'health' ) ) { echo 'class="current"'; } ?>>
+                    <a href="<?php echo cc_aha_get_analysis_permalink(); ?>">Health Analysis Report</a>
+                </li>
+				<li <?php if ( cc_aha_on_analysis_screen( 'revenue' ) ) { echo 'class="current"'; } ?>>
+					<a href="<?php echo cc_aha_get_analysis_permalink( 'revenue' ); ?>">Revenue Analysis Report</a>
+				</li>
+				-->
+				Testing nav tabs
+            </ul>
+        </div>
+        <?php
+}
+
