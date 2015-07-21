63d1a5ab8a4f1834bb9a096626c9b52f0c019fb9
Melissa De Bartolomeo <debartolomeo@hotmail.com>
2015-07-21 11:44:03 -0500

    outlines of lookup functions w field names sorted added

--- includes/cc-transtria-database-bridge.php
+++ includes/cc-transtria-database-bridge.php
@@ -125,16 +125,173 @@ function cc_transtria_get_endnote_id_title( ){
 
 //TODO: functions for lookups for fields...
 
+/**
+ * Function to get form drop down options.  First pass takes input array as lookup and foreachs..
+ *
+ * @return Array. Array of arrays.
+ */
+function cc_transtria_get_dropdown_options( ){
+
+	//list of dropdowns with single instance on the form (i.e., no other dropdowns of that type exist)
+	$dd_options_singletons = cc_transtria_get_singleton_dropdown_options();
+	
+	//list of dropdowns with multiple instances (Populations tabs)
+	$dd_options_multiples_pops = cc_transtria_get_multiple_dropdown_options_populations();
+	
+	//list of dropdowns with multiple instances (EA tabs)
+	$dd_options_multiples_ea = cc_transtria_get_multiple_dropdown_options_ea();
+
+
+
+
+}
 
 /**
- * 
+ * Gets an array of SINGLETON lookups to do and does those lookups
  *
- *
+ * @return Array.  Array of arrays of options for all singleton dropwdown fields (dropdowns that only exist ONCE in the form).
  *
  */
+function cc_transtria_get_singleton_dropdown_options(){
 
- 
- 
+	$dd_ids = cc_transtria_get_singleton_dropdown_ids();
+	
+	//Now, perform lookup.
+
+
+}
+
+/**
+ * Gets an array of MULTIPLE lookups for POPULATIONS  and does those lookups
+ *
+ * @return Array.  Array of arrays of options for all multiple POPULATION dropwdown fields (dropdowns that exist in Populations tabs in form).
+ *
+ */
+function cc_transtria_get_multiple_dropdown_options_populations(){
+
+	$dd_ids = cc_transtria_get_multiple_dropdown_ids_populations();
+	
+	//Now, perform lookup.
+
+
+}
+
+/**
+ * Gets an array of SINGLETON lookups and does those lookups
+ *
+ * @return Array.  Array of arrays of options for all singleton dropdown fields (dropdowns that only exist ONCE in the form).
+ *
+ */
+function cc_transtria_get_multiple_dropdown_options_ea(){
+
+	$dd_ids = cc_transtria_get_multiple_dropdown_ids_ea();
+	
+	//Now, perform lookup.
+
+
+}
+
+
+
+/******** LOOKUP TABLES ARE FUN *********/
+
+/**
+ * Returns array of div ids -> code_tbl lookup names for singleton dropdown fields in form.
+ *
+ * @return array. String div id in form => String lookup name in db (code_tbl)
+ *
+ */
+function cc_transtria_get_singleton_dropdown_ids(){
+
+	$dd_ids = array(
+		'searchtooltype' => 'SearchToolType',
+		'searchtoolname' => 'SearchToolName',
+		"fundingsource" => "FundingSource",
+		"domesticfundingsources" => "domesticfundingsources",
+		"fundingpurpose" => "fundingpurpose",
+		'StudyDesign' => 'StudyDesignID',
+		"validity_threats"=> "ValidityThreats",
+		"ipe_exposure_frequency" => "ipe.ExposureFrequency",  //only on IPE tab.  TODO: confirm!
+		"state_setting" => 'US States',
+		"setting_type" => "SettingType",
+		"partner_discipline" => "Partner discipline",
+		"theory_framework_type" => "TheoryFramework",
+		"intervention_component" => "Intervention Components",
+		"strategies" => "Strategies",
+		"pse_components" => "PSEcomponents",
+		"complexity" => "Complexity",
+		"intervention_location" => "InterventionLocation",
+		"intervention_indicators" => "Indicator",
+		"intervention_outcomes_assessed" => "OutcomesAccessed",
+		"evaluation_type" => "EvaluationType",
+		"evaluation_methods" => "EvaluationMethod"
+		
+
+
+	);
+
+
+
+}
+
+/**
+ * Returns array of div ids -> code_tbl lookup names for multiple dropdown fields in form for Populations tabs.
+ *
+ * @return array. String div id in form => String lookup name in db (code_tbl)
+ *
+ */
+function cc_transtria_get_multiple_dropdown_ids_populations(){
+
+	//get basic field and lookup names
+	$dd_ids = array(
+		"_geographic_scale" => "GeographicScale", //"%s_geographic_scale" % _prefix, "%s.GeographicScale"  %s is the pop prepend (ese0, tp, ipe...)
+		"_gender" => "GenderCode",
+		//"ipe_exposure_frequency" => "ipe.ExposureFrequency",  //TODO: special case...\
+		"_sub_populations" => "SubPopulations",
+		"_youth_populations" => "YouthPopulations",
+		"_professional_populations" => 'ProfessionalPopulations',
+		//TODO: figure this mess out.
+		//ESE and IPE only, but there are multiple ESE tabs, awesoooome
+		"_representative_subpopulations" => "RepresentativeSubpopulations",
+		//ESE and IPE only (all ESE tabs)
+		"_subpopulations" => "HighRiskSubpopulations"
+		
+		
+	
+	);
+	
+	//
+
+}
+
+/**
+ * Returns array of div ids -> code_tbl lookup names for multiple dropdown fields in form for EA tabs.
+ *
+ * @return array. String div id in form => String lookup name in db (code_tbl)
+ *
+ */
+function cc_transtria_get_multiple_dropdown_ids_ea(){
+
+	$dd_ids = array(
+		"_duration" => "_duration", //"%s_duration" % _prefix, "%s_duration"
+		"_result_evaluation_population" => " Results Populations", //"%s_result_evaluation_population" % _prefix, "%s Results Populations"
+		"_result_subpopulations" => " Results SubPopulations", //"%s_result_subpopulations" % _prefix, "%s Results SubPopulations"
+		"_result_indicator_direction"=> "indicator_direction", //"%s_result_indicator_direction" % _prefix, "indicator_direction"
+		"_result_outcome_direction" => "outcome_direction", //"%s_result_outcome_direction" % _prefix, "outcome_direction"
+		"_result_strategy" => "result_strategy", //"%s_result_strategy" % _prefix, "result_strategy"
+		"_result_outcome_type" => '%s_result_outcome_type_other', //"%s_result_outcome_type" % _prefix, '%s_result_outcome_type_other'
+		//yes, this is a typo.  No we're not changing it now, it's in the db.
+		"_result_outcome_accessed" => " OutcomesAccessed", //"%s_result_outcome_accessed" % _prefix, "%s OutcomesAccessed" 
+		"_result_measures" => " Measures", //"%s_result_measures" % _prefix, "%s Measures"
+		"_result_indicator" => " Indicator", //"%s_result_indicator" % _prefix, "%s Indicator"
+		"_result_statistical_model" => "statistical_model", //"%s_result_statistical_model" % _prefix, "statistical_model"
+		"_result_statistical_measure" => "statistical_measure" //"%s_result_statistical_measure" % _prefix, "statistical_measure"
+		
+
+
+	);
+
+}
 
 
 /***** EXAMPLES FROM AHA ******/
